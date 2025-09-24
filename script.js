08736aef4cd43beca0d9d9ae55c934e7c1411c46
Lalartha <lalartha317@gamil.com>
2025-09-25 00:59:24 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("calculateBtn").onclick=calculateExpense;
+function calculateExpense() {
+    let materialCost = parseInt(document.getElementById("materials").value);
+    let quantity = parseInt(document.getElementById("quantity").value);
+    let locationrent = parseInt(document.getElementById("location").value);
+    let total=(materialCost * quantity)+locationrent;
+    document.getElementById("result").innerHTML = "Total Expense: " + total;
+    console.log("Button clicked!")
+}
+function menuToggle(){
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
